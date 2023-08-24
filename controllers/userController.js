2775aa558de7a5a37521904174d3210bbb5a87e3
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-23 22:54:06 -0500

    userController.js get all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,29 @@
 // **`/api/users`**
+// ObjectId() method for converting userId string into an ObjectId for querying database
+const { ObjectId } = require("mongoose").Types;
+const { default: mongoose } = require("mongoose");
+const { Users, Thoughts } = require("../models");
 // * `GET` all users
+const userCount = async () => {
+  try {
+    const userData = await Users.aggregate([
+      {
+        $group: {
+          _id: null,
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    if (numberofUsers.length > 0) {
+      return numberofThoughts[0].count;
+    } else {
+      return 0;
+    }
+  } catch (err) {
+    console.error("Error while aggregating thought data:", error);
+    throw error;
+  }
+};
 // * `GET` a single user by its `_id` and populated thought and friend data
 
 // * `POST` a new user:
