221cffe1eaed9d37e0dbb3d6d2c38eede5e717c9
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-23 22:39:43 -0500

    thoughtRouter.js

--- routes/api/thoughtRouter.js
+++ routes/api/thoughtRouter.js
@@ -1,1 +1,27 @@
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThoughtbyId,
+  deleteThoughtbyId,
+  addReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController.js");
 
+// /api/thoughts
+
+router.route("/").get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getThoughtById)
+  .put(updateThoughtbyId)
+  .delete(deleteThoughtbyId);
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
